dab2e4f61cb1ac39394e130a75a4ba3b2e9bd69f
NepuShiro <nepushiro@nepunep.xyz>
2024-11-26 20:41:12 -0600

    Update AssemblyInfo

--- PROPERTIES/AssemblyInfo.cs
+++ PROPERTIES/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("ResoTabbed")]
 [assembly: AssemblyProduct("ResoTabbed")]
-[assembly: AssemblyDescription("Description of your mod")]
-[assembly: AssemblyCopyright("Copyright © 2024 ExampleAuthor")]
+[assembly: AssemblyDescription("Adds a Dynamic Variable to show when you're tabbed in or not")]
+[assembly: AssemblyCopyright("Copyright © 2024 NepuShiro")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
